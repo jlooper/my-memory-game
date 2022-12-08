6b5ba6410abccbd006b015964425e9de8285fc5a
Jen Looper <jen.looper@gmail.com>
2022-12-07 21:14:31 -0500

    Initial commit

---
+++ src/components/CardView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="`view ${viewType}-view`">
+    <img :src="imageUrl" :alt="imageAltText" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    viewType: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      default: `/que_icon.svg`,
+    },
+    imageAltText: {
+      type: String,
+      default: `hidden card`,
+    },
+  },
+};
+</script>
---
+++ src/data/memoryCards8.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    url: "./img-1.png",
+    altText: "An emerald cut into a diamond shape."
+  },
+  {
+    url: "./img-2.png",
+    altText: "A ruby cut into a pentagon shape."
+  },
+  {
+    url: "./img-3.png",
+    altText: "An amethyst in a princess cut."
+  },
+  {
+    url: "./img-4.png",
+    altText: "A rose quartz stone cut into a pentagon shape."
+  },
+  {
+    url: "./img-5.png",
+    altText: "A sapphire cut into a hexagon shape."
+  },
+  {
+    url: "./img-6.png",
+    altText: "An amethyst cut into a diamond shape."
+  },
+  {
+    url: "./img-7.png",
+    altText: "A topaz cut into a hexagon shape."
+  },
+  {
+    url: "./img-8.png",
+    altText: "An emerald cut into a hexagon shape."
+  }
+];
